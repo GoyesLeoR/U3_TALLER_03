7b4f8ad90cdf14ba487e403fa9f1faa2ee9311ac
GoyesLeoR <rgoyesr@est.ups.edu.ec>
2021-03-04 01:28:50 -0500

    Se corrigio errores en el routes.js

--- code/network/routes.js
+++ code/network/routes.js
@@ -3,14 +3,14 @@ const { route } = require('../components/usuario/network')
 const usuario = require('../components/usuario/network')
 const carrera = require('../components/carrera/network')
 const institucion = require('../components/institucion/network')
-const representate_legal = require('../components/representate_legal/network')
+const representante_legal = require('../components/representante_legal/network')
 
 
 const routes = function( server ) {
     server.use('/usuario', usuario)
     server.use('/carrera', carrera)
     server.use('/institucion', institucion)
-    server.use('/representate_legal', representate_legal)
+    server.use('/representante_legal', representante_legal)
 }
 
 module.exports = routes
